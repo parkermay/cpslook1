3ae30558f701cd334285a88af4b71c621ec5dec3
Parker May <parker.may@mckesson.com>
2017-07-07 15:40:37 -0400

    Save button goes to debugger

--- src/app/configuration/filter/filter.component.ts
+++ src/app/configuration/filter/filter.component.ts
@@ -42,8 +42,12 @@ export class FilterComponent implements OnInit {
     }
 
     performSearch(): void {
-        
         console.log(`filter = ${JSON.stringify(this.filter)}`);
+        debugger;
+    }
+
+    saveChanges(): void {
+        debugger;
     }
 
     selectSavedFilter(commonOption: string): void {
